553bc59364ef1ea7f430993fad689621245ea991
dex06 <derek.samson@etu.unice.fr>
2021-02-03 04:40:14 -0800

    first commit

---
+++ forum/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  forum
+//
+//  Created by Derek Samson on 03/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
